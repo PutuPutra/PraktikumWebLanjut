cc0bf83cdc17696223e909d5bbf75706e6d822e9
Putu Putra Eka Persada <83483873+PutuPutra@users.noreply.github.com>
2022-09-20 19:40:26 +0700

    Fourth Week

---
+++ Pertemuan 4/app/Views/pages/Home.php
@@ -0,0 +1,4 @@
+<hi>ini halaman home</hi>
+<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni tempora minima aliquid sint commodi provident,
+    delectus rerum possimus excepturi ea.
+</p>
